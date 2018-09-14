ba94324c0acc0830d61e67daf253a4e28238bfa7
TRACY S MUSIKER <tracymusicmaker@gmail.com>
2018-09-14 16:12:50 -0400

    latest birds, more functional

--- public/main.js
+++ public/main.js
@@ -1,17 +1,60 @@
 let birdsDisplay = document.querySelector('.birds');
-let addBirdForm = document.querySelector('.add-a-bird');
-let input = document.querySelector('.new-birds');
+let addBirdForm = document.querySelector('.bird-form');
+let nameInput = document.querySelector('.name');
+let colorAddInput = document.querySelector('.add-more-colors')
 
 
-
-
-let addBird = (event) => {
-    event.preventDefault();
-    input.value
-
+let displayBirds = (data) => {
+    localStorage.setItem('birdInfo', JSON.stringify(data));
+    console.log(data);
+    for (bird in data) {
+        console.log(bird);
+        let birdDisplay = document.createElement('div');
+        birdDisplay.textContent = bird + " " + (data[bird].color || 'colorless bird');
+        birdsDisplay.appendChild(birdDisplay);
+    }
 }
 
-addBirdForm.addEventListener('submit', addBird)
+let createColorInputs = () => {
+    let colors = document.querySelector('.colors')
+    let newColorInput = document.createElement('input');
+    newColorInput.classList.add('color');
+    newColorInput.setAttribute('type', 'text');
+    newColorInput.setAttribute('placeholder', 'type color here');
+    colors.appendChild(newColorInput);
+}
+
+
+colorAddInput.addEventListener('click', createColorInputs);
+
+
+let addColor = (event) => {
+    event.preventDefault();
+    let colorInputs = document.querySelectorAll('.color')
+    let colors = [];
+    for (colorInput of colorInputs) {
+        colors.push(colorInput.value);
+    } 
+    let data = {
+        name: nameInput.value,
+        color: colors
+    }
+    console.log(data);
+    console.log(JSON.stringify(data));
+    fetch('/colors', {
+        method: 'post',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(displayBirds)
+    // input.value
+}
+
+
+addBirdForm.addEventListener('submit', addColor)
 
 
 fetch('/test')
@@ -21,11 +64,4 @@ fetch('/test')
 
 fetch('/colors')
     .then(res => res.json())
-    .then(data => {
-        // birds.textContent = data.bird + data[0].color;
-        for (bird of data) {
-            let birdDisplay = document.createElement('div');
-            birdDisplay.textContent = bird.bird + " " + (bird.color || "");
-            birdsDisplay.appendChild(birdDisplay);
-        }
-    });
+    .then(displayBirds);
--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 
 
-let birds = [
-    {bird: 'macaw', color: ['red', 'green', 'yellow']}
-]
+let birds = {
+    'macaw': {bird: 'macaw', color: ['red', 'green', 'yellow']}
+}
 
 
 let goHome = (req, res) => {
@@ -19,8 +19,17 @@ let birdFeatherColors = (req, res) => {
     res.send(birds)
 }
 
+
+
+let postFood = (req, res) => {
+    birds[req.body.name].food = req.body.food;
+    res.send(birds);
+}
+
 let postColors = (req, res) => {
-    birds.push(req.body);
+    console.log(req.body);
+    !birds[req.body.name] && (birds[req.body.name] = {});
+    birds[req.body.name].color = req.body.color 
     res.send(birds);
 }
 
@@ -31,6 +40,7 @@ app.use(bodyParser.json());
 app.get('/test', goHome);
 app.get('/colors', birdFeatherColors);
 app.post('/colors', postColors);
+app.post('/food', postFood);
 
 // @end
 app.listen(3000);
